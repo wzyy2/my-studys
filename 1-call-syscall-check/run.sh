24851fe8b11f6e694a3624c11d61b75d5c61960e
Jacob Chen <yanliu.ch@cainiao.com>
2022-09-21 14:42:30 +0800

    udpate

--- 1-call-syscall-check/run.sh
+++ 1-call-syscall-check/run.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-echo -e "$(tput setaf 1)测试用户程序$(tput setaf 0)"
+echo -e "$(tput setaf 1)测试用户程序$(tput setaf 7)"
 LD_PRELOAD=./build/libs/syscall-stats/libsyscall-stats.so  ./build/app/app
 
-echo -e "$(tput setaf 1)测试ls$(tput setaf 0)"
+echo -e "$(tput setaf 1)测试ls$(tput setaf 7)"
 LD_PRELOAD=./build/libs/syscall-stats/libsyscall-stats.so  ls
 
-echo -e "$(tput setaf 1)测试nc$(tput setaf 0)"
+echo -e "$(tput setaf 1)测试nc$(tput setaf 7)"
 LD_PRELOAD=./build/libs/syscall-stats/libsyscall-stats.so  nc -w 1 127.0.0.1 22
